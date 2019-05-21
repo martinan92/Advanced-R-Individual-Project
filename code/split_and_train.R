d4f0eeaa69ac4d42659d71f9cb6f6ff8f624ad29
Andrew Martinez <andrew.j.martinez@student.ie.edu>
2019-05-21 19:35:46 +0200

    New function needs import for f_partition

--- code/split_and_train.R
+++ code/split_and_train.R
@@ -1,3 +1,5 @@
+source('code/f_partition.R')
+
 split_and_train <- function(df, seed = 123456){
   split_fe_train_df <- f_partition(df, test_proportion = 0.2, seed = seed)
   fe_rf <- ranger(formula = as.formula(price~.), data=split_fe_train_df$train[,!c('id')])
