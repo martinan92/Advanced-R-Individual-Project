433d9cb4e755c6f011a368e78928aac4763cd920
Andrew Martinez <andrew.j.martinez@student.ie.edu>
2019-05-22 12:51:10 +0200

    Added impurity variable importance

--- code/split_and_train.R
+++ code/split_and_train.R
@@ -2,7 +2,7 @@ source('code/f_partition.R')
 
 split_and_train <- function(df, seed = 123456){
   split_fe_train_df <- f_partition(df, test_proportion = 0.2, seed = seed)
-  fe_rf <- ranger(formula = as.formula(price~.), data=split_fe_train_df$train[,!c('id')])
+  fe_rf <- ranger(formula = as.formula(price~.), data=split_fe_train_df$train[,!c('id')], importance = 'impurity')
   print(fe_rf)
   
   fe<-predict(fe_rf, data = split_fe_train_df$test, type='response')$predictions
